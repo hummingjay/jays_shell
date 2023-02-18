d238d32bfec67a9d677748354af8da50318e6a48
hummingjay <jays7mill@gmail.com>
2023-02-18 03:23:16 -0800

    V2 exec

--- exec2.c
+++ exec2.c
@@ -3,7 +3,8 @@
 #include <sys/wait.h>
 /**
  * main - improvement of exec.c by now using wait
- * to ensure parent finishes running after execv
+ * to ensure parent finishes running after execve
+ * which will run in the child
  *
  * Return:0
  */
@@ -11,12 +12,12 @@ int main(void)
 {
 	pid_t pid;
 	int val;
-	char argv[] = {"/bin/ls", "-l", NULL};
+	char *argv[] = {"/bin/ls", "-l", NULL};
 
 	pid = fork();
 	if (pid == -1)
 	{
-		perror("you ain't forked nothin");
+		perror("you ain't forked nothin\n");
 		return (0);
 	}
 	if (pid == 0)
@@ -25,14 +26,14 @@ int main(void)
 		val = execve(argv[0], argv, NULL);
 		if (val == -1)
 		{
-			printf("Didn't execute a thing");
+			printf("Didn't execute a thing\n");
 			return (0);
 		}
 	}
 	else
 	{
 		wait(NULL);
-		printf("Done executing");
+		printf("Done executing\n");
 	}
 	return (0);
 }
